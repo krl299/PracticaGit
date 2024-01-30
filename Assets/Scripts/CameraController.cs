d55f79fd29d0d826c0bedcdc1eacb27e161aa635
PeliAzuh <47522008+krl299@users.noreply.github.com>
2024-01-30 16:09:34 +0100

    comments and clean methods on script

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,93 +5,107 @@ using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
-    public float moveSpeed;
-    [Header("Rotate config")]
-    public float minXRot;
-    public float maxXRot;
-    private float curXRot;
-    public float rotateSpeed;
+    private InputSystem inputActions;
+    [SerializeField] private Camera gameCamera;
+    [SerializeField] private float cameraMovementSpeed;
+    [SerializeField] private Vector2 moveDir;
 
-    [Header("Zoom config")]
-    public float minZoom;
-    public float maxZoom;
-    public float zoomSpeed;
+    [Header("Camera Zoom")]
+    [SerializeField] private float zoomSpeed;
+    [SerializeField] private float minZoom;
+    [SerializeField] private float maxZoom;
+    [SerializeField] private float currentZoom;
 
-    private float curZoom;
-    // reference to the camera object
-    private Camera cam;
+    [Header("Camera Rotation")]
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float minRotation;
+    [SerializeField] private float maxRotation;
+    [SerializeField] private Vector2 currentRotation;
+    [SerializeField] private float currentRotationX;
+    [SerializeField] private bool isRotating;
+    
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// It initializes the game camera, input actions, and sets up the event handlers for player actions.
+    /// </summary>
+    private void Awake()
+    {
+        gameCamera = GetComponentInChildren<Camera>();
 
-    private float mouseX;
+        inputActions = new InputSystem();
 
-    //Indicates if the camera is rotating
-    private bool rotating;
+        inputActions.Player.Move.performed += ctx => moveDir = ctx.ReadValue<Vector2>();
+        inputActions.Player.Move.canceled += ctx => moveDir = Vector2.zero;
+        inputActions.Player.Zoom.performed += ctx => currentZoom += ctx.ReadValue<Vector2>().y * zoomSpeed;
+        inputActions.Player.Rotate.performed += ctx => isRotating = true;
+        inputActions.Player.Rotate.canceled += ctx => isRotating = false;
+        inputActions.Player.Look.performed += ctx => currentRotation = ctx.ReadValue<Vector2>();
+        inputActions.Player.Look.canceled += ctx => currentRotation = Vector2.zero;
+        inputActions.Player.Enable();
+    }
 
-    private Vector2 moveDirection;
-
-
+    /// <summary>
+    /// Start is called before the first frame update.
+    /// It sets the initial zoom and rotation of the camera.
+    /// </summary>
     private void Start()
     {
-        cam = Camera.main;
-        curZoom = cam.transform.localPosition.y;
-
-        curXRot = -50;
-
+        currentZoom = 45;
+        currentRotationX = -50;
     }
 
-    public void OnZoom(InputAction.CallbackContext context)
-    {
-        //getting the scroll wheel value
-        curZoom += context.ReadValue<Vector2>().y * zoomSpeed;
-        //clamping it between min/max zoom values
-        curZoom = Mathf.Clamp(curZoom, minZoom, maxZoom);
-    }
-
-
-    public void OnRotateToggle(InputAction.CallbackContext context)
-    {
-        rotating = context.ReadValueAsButton();
-        if (!rotating) mouseX = 0; //To avoid infinite spin
-    }
-
-    public void OnRotate(InputAction.CallbackContext context)
-    {
-        //If clicking right button mouse
-        if(rotating)
-        {
-            mouseX = context.ReadValue<Vector2>().x;
-            float mouseY = context.ReadValue<Vector2>().y;
-
-            curXRot += -mouseY * rotateSpeed;
-            curXRot = Mathf.Clamp(curXRot, minXRot, maxXRot);                   
-        }
-    }
-
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        //Read the input value that is being sent by the Input system
-        moveDirection = context.ReadValue<Vector2>();
-    }
-
+    /// <summary>
+    /// LateUpdate is called once per frame, after all Update functions have been called.
+    /// It moves, zooms, and rotates the camera.
+    /// </summary>
     private void LateUpdate()
     {
-        //applying ZOOM to the camera
-        cam.transform.localPosition = Vector3.up * curZoom;
+        MoveCamera(moveDir);
+        ZoomCamera();
+        RotateCamera();
+    }
 
-        //applying ROTATION to the anchor
-        transform.eulerAngles = new Vector3(curXRot, transform.eulerAngles.y + (mouseX * rotateSpeed), 0.0f);
+    /// <summary>
+    /// Rotates the camera based on mouse input.
+    /// </summary>
+    private void RotateCamera()
+    {
+        float mouseX;
+        if (!isRotating)
+        {
+            mouseX = 0;
+            return;
+        }
+        mouseX = currentRotation.x;
+        float mouseY = currentRotation.y;
+        currentRotationX += -mouseY * rotationSpeed; 
+        currentRotationX = Mathf.Clamp(currentRotationX, minRotation, maxRotation);
+        transform.eulerAngles = new Vector3(currentRotationX,
+            transform.eulerAngles.y + (mouseX * rotationSpeed), 0);
+    }
 
-        //applying MOVEMENT to the camera
-        Vector3 forward = cam.transform.forward;
-        forward.y = 0.0f;
+    /// <summary>
+    /// Adjusts the camera's zoom level.
+    /// </summary>
+    private void ZoomCamera()
+    {
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+        if (!gameCamera.orthographic)
+            gameCamera.fieldOfView = currentZoom;
+    }
+
+    /// <summary>
+    /// Moves the camera based on the input vector.
+    /// </summary>
+    /// <param name="inputVector">The input vector, where x represents horizontal movement and y represents vertical movement.</param>
+    public void MoveCamera(Vector2 inputVector)
+    {
+        Vector3 forward = gameCamera.transform.forward;
+        forward.y = 0;
         forward.Normalize();
+        Vector3 right = gameCamera.transform.right;
+        Vector3 dir = forward * inputVector.y + right * inputVector.x;
 
-        Vector3 right = cam.transform.right;
-
-        //get a local direction
-        Vector3 dir = forward * moveDirection.y + right * moveDirection.x;
-
-        dir *= moveSpeed * Time.deltaTime;
-
-        transform.position += dir;
+        transform.position += dir * Time.deltaTime * cameraMovementSpeed;
     }
 }
