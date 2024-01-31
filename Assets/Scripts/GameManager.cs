0d5f16274e7787197c4bd4e5dac805440580be9a
Carlos Morán <kr2moran99@gmail.com>
2024-01-31 09:26:41 +0100

    singleton on gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Game Manager is a singleton class that manages the game state.
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
